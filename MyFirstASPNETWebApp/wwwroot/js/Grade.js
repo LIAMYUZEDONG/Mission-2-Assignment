2878124b3defc52ef3a5b4d117ff0d8ac6de241e
LIAMYUZEDONG <yyyzzddd@gmail.com>
2022-01-12 18:10:42 -0700

    M2 assignment

---
+++ MyFirstASPNETWebApp/wwwroot/js/Grade.js
@@ -0,0 +1,9 @@
+﻿
+$("#btnSend").click(function () {
+    alert("Assignment Percentage: " + $("#txtAssignmentPercentage").val() +
+        ", Group Project Percentage: " + $("#txtGroupProjectPercentage").val() +
+        ", Quizzes Percentage: " + $("#txtQuizzesPercentage").val() +
+        ", Exams Percentage: " + $("#txtExamsPercentage").val() +
+        ", INTEX Percentage: " + $("#txtINTEXPercentage").val()
+    )
+})
